0c5cf981ad7d4f972be4761488aeb51470b5d411
Div@JDY <48310083+DivJDY@users.noreply.github.com>
2022-06-11 08:55:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import "./App.css";
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import history from "./history";
 import Navigation from "./Navigation";
 import FoodList from "./Components/FoodList";
 import FoodTable from "./Components/FoodTable";
+import FoodDescription from "./Components/foodDescription";
 
 function App() {
   return (
     <div className="container">
-      <Router>
-        {/* Navigation */}
+      <Router history={history}>
         <Navigation className="w-25" />
         <div className="w-75">
           <Routes>
@@ -24,6 +19,9 @@ function App() {
 
           <Routes>
             <Route path="/foodTable" element={<FoodTable />} exact />
+          </Routes>
+          <Routes>
+            <Route path="/foodDesc" element={<FoodDescription />} exact />
           </Routes>
         </div>
       </Router>
